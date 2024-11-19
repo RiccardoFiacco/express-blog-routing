80033cb58c4778085a3fd66d1f29189371c4e59a
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-19 14:30:19 +0100

    add listen function

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const port = 3000; //attribuiamo una porta
 const posts = require('./posts'); //importiamo i post
 
 app.use(express.static('public'));//rendiamo il contenuto della cartella public publico
+
+
+app.listen(port, ()=>{ //mettiamo il server in ascolto alla porta numero 3000
+    console.log(`in ascolto sulla porta ${port}`)
+})
